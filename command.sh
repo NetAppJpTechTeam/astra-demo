1d18be1fa9ccda69d9620869ce47c3c1a34736d4
yoichi <yoichi.hokayama@netapp.com>
2023-05-22 14:19:44 +0900

    20230522_003

--- command.sh
+++ command.sh
@@ -7,9 +7,11 @@ SVM_NAME_SRC=svm1
 SVM_NAME_DEST=svmbk1
 AGGR_NAME=soc_ontap_01_SSD_1
 TMP_VOL_NAME_DEST=dest_sm01
+TMP_CLONE_NAME=fvolume01
 VOL_SIZE=10Gi
 
 # Kubernetes / TRIDENT INFO
+NAMESPACE=default
 POD_NAME=catalog-mysql-0
 BACKEND_NAME=bk-ontapnas
 TRIDENT_PREFIX="trident_svm1_34_"
@@ -25,11 +27,18 @@ VOL_NAME_SRC=$TRIDENT_PREFIX`echo $PV_NAME_SRC|sed -e s/-/_/g`
 ### COMMAND ###
 ### ---------
 
-# Ontapコマンド生成
+# Ontapコマンド生成: snapmirror
 echo "volume create -vserver $SVM_NAME_DEST -aggregate $AGGR_NAME -type DP -size 10g -volume $TMP_VOL_NAME_DEST"
 echo "snapmirror initialize -destination-path $SVM_NAME_DEST:$TMP_VOL_NAME_DEST -source-path $SVM_NAME_SRC:$VOL_NAME_SRC"
 echo "snapmirror show -destination-path $SVM_NAME_DEST:$TMP_VOL_NAME_DEST -source-path $SVM_NAME_SRC:$VOL_NAME_SRC"
 echo "snapmirror break -destination-path $SVM_NAME_DEST:$TMP_VOL_NAME_DEST -source-path $SVM_NAME_SRC:$VOL_NAME_SRC"
+
+# Ontapコマンド生成: clone / rehost
+echo `volume clone create -parent-volume $VOL_NAME_SRC -vserver $SVM_NAME_SRC -flexclone $TMP_CLONE_NAME`
+echo `volume clone split start -vserver $SVM_NAME_SRC -flexclone $TMP_CLONE_NAME`
+echo `volume rehost -vserver $SVM_NAME_SRC -volume $TMP_CLONE_NAME -destination-vserver $SVM_NAME_DEST`
+
+
 
 # Trident コマンド生成
 echo "tridentctl -n trident import volume $BACKEND_NAME $TMP_VOL_NAME_DEST -f $TRIDENT_IMPORT_FILE"
@@ -40,7 +49,7 @@ apiVersion: v1
 kind: PersistentVolumeClaim
 metadata:
   name: $PVC_NAME
-  namespace: default
+  namespace: $NAMESPACE
 spec:
   accessModes:
     - ReadWriteOnce
